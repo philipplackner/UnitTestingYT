3ea7fe99cbd628e11d8afc5f567ad32598047133
Philipp <mike.litoris@gmail.com>
2020-08-11 22:39:40 +0200

    Added ResourceComparer.kt and ResourceComparerTest.kt

---
+++ app/src/androidTest/java/com/androiddevs/unittestingyt/ResourceComparerTest.kt
@@ -0,0 +1,32 @@
+package com.androiddevs.unittestingyt
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "UnitTestingYT")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Hello")
+        assertThat(result).isFalse()
+    }
+}
---
+++ app/src/main/java/com/androiddevs/unittestingyt/ResourceComparer.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.unittestingyt
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(context: Context, resId: Int, string: String): Boolean {
+        return context.getString(resId) == string
+    }
+}
